3977dc199a731b6827fd23b7d9e650003d9f029f
mariadias143 <marianovadias@gmail.com>
2020-04-28 18:33:41 +0100

    base working

--- Poisson/poisson.cpp
+++ Poisson/poisson.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <cstdlib>
 #include <cmath>
+#include <math.h>
 #include <string.h>
 #include <limits>
 #include <algorithm>
@@ -11,12 +12,12 @@
 #include <stdlib.h>
 #include <sys/time.h>
 
+using namespace std;
+
 #define TIME_RESOLUTION 1000000
 
 long long unsigned initial_time;
 timeval t;
-//Parâmetro de relaxação
-double p = 2/(1+sin(pi/(N-1)));
 
 void start (void) {
 	gettimeofday(&t, NULL);
@@ -54,38 +55,29 @@ int computeDifference(int * u, int * w, int n){
     for(int j=0; j<n; j++)
       diff[i*n+j] = abs(w[i*n+j] - u[i*n+j]);
     
-  int max = std::max_element(diff, diff + n*n);
+  int max = *max_element(diff, diff + n*n);
   return max;
 }
 
+void compare(int* w, int* u, int n){
+  for(int i=0; i<n; i++)
+    for(int j=0; j<n; j++){
+      if(w[i*n + j] != u[i*n + j]){
+        std::cout << "i:" << i << " j:" << j << " w:" << w[i*n + j] << " u:" << u[i*n + j] << std::endl;
+        exit(0);
+      }
+    }
+}
 
-void poisson(int n, int tol){
+int poisson(int n, int tol, double p, int * w, int * u){
 	long long unsigned tt;
-
-  int * w = new int[n*n]; // values of current iteration
-  int * u = new int[n*n]; // last iteration values
-
-  // Temperature is zero at the top and 100 on the other boundaries
-  for(int i=0; i<n; i++){
-    grid[i] = 0; //above border
-    grid[i + n*(n-1)] = 100; //lower border
-  }
-
-  for(int j=0; j<n; j++){
-    grid[j*n] = 100; //left border
-    grid[n-1 + j*n] = 100; //right border
-  }
-
-  for(int i=1; i<n-1; i++)
-    for(int j=1; j<n-1; i++)
-      grid[i*n + j] = 50; //initial aproximation for interior points
-  
 
   int diff = tol + 1;
   int n_iter = 0;
   while(diff > tol){
-    std::copy(std::begin(w), std::end(w), std::begin(u)); // u = w;
-
+    std::copy(w, w + n*n, u); // u = w;
+    //compare(w,u,n);
+    
     // Update red
     for(int i=1; i<n-1; i++)
       for(int j=1+remainder(i,2); j<n-1; j+=2)
@@ -97,10 +89,8 @@ void poisson(int n, int tol){
         w[i*n + j] = (1-p)*w[i*n + j] + p * ( w[(i-1)*n+j] + w[i*n+(j-1)] + w[(i+1)*n+j] + w[i*n+(j+1)] ) / 4;
 
     n_iter++;
-    
+    diff = computeDifference(u,w,n);
   }
-
-  diff = computeDifference(u,w,n);
 
   return n_iter;
 }
@@ -108,10 +98,34 @@ void poisson(int n, int tol){
 
 int main(int argc, char const *argv[]) {
 
-  int n = atoi(argv[1]);
-  int tol = atoi(argv[2]);
+  int n = 100; //atoi(argv[1]);
+  int tol = 10; //atoi(argv[2]);
 
-  poisson(n,tol);
+  //Parâmetro de relaxação
+  double p = 2/(1+sin(M_PI/(n-1)));
+
+  int * w = new int[n*n]; // values of current iteration
+  int * u = new int[n*n]; // last iteration values
+
+  // Temperature is zero at the top and 100 on the other boundaries
+  for(int i=0; i<n; i++){
+    w[i] = 0; //above border
+    w[i + n*(n-1)] = 100; //lower border
+  }
+
+  for(int j=0; j<n; j++){
+    w[j*n] = 100; //left border
+    w[n-1 + j*n] = 100; //right border
+  }
+
+  for(int i=1; i<(n-1); i++){
+    for(int j=1; j<(n-1); j++){
+      w[i*n + j] = 50; //initial aproximation for interior points
+    }
+  }
+
+  int res = poisson(n,tol,p,w,u);
+  cout << "Iteraçoes: " << res << endl;
 
   return 0;
 }
